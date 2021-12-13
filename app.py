1d3f88989a4442b33ea0c8fa1b9c12be6efaee9b
Yuan Zhong <yzhong4@mdanderson.org>
2021-12-13 05:16:36 -0600

    updated climate_starter

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
+# 1. Import Flask
 from flask import Flask
-
+# 2. Create an app
 app = Flask(__name__)
-
+# 3. Define static routes
 @app.route("/")
-def hello():
-    return "Hello, World!"
+def index():
+    return(f"/api/v1.0/precipitation"
+             "/api/v1.0/precipitation")
+    # return(f"/api/v1.0/stations")
+    # return(f"/api/v1.0/tobs")
+    # return(f"/api/v1.0/<start> and /api/v1.0/<start>/<end>")
+
+# 4. Define main behavior
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
+
